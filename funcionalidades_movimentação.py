bfa435f787be72f43ef4dbc6477fae5f9d7012c4
Thedark182 <mathzzz.calegari@gmail.com>
2026-02-27 01:26:34 -0300

    funcionalidades_movimentação.py

--- funcionalidades_movimentação.py
+++ funcionalidades_movimentação.py
@@ -1,3 +1,3 @@
-#Funcionalidades de movimentação do personagem 'Noel'
-#Implementação de funcionalidades de movimentação conforme exigências do projeto
-#Mudanças realizadas 27/02
+Funcionalidades de movimentação do personagem 'Noel'
+Implementação de funcionalidades de movimentação conforme exigências do projeto
+Mudanças realizadas 27/02
